8f00efb1772871be3f91ee36b713ddc43c3a9210
Don Li <dli9431@gmail.com>
2020-02-25 12:37:33 -0800

    final product

--- src/index.js
+++ src/index.js
@@ -23,22 +23,21 @@ class Board extends React.Component {
 	render() {
 		return (
 			<div>
-			<div className="status">{status}</div>
-			<div className="board-row">
-				{this.renderSquare(0)}
-				{this.renderSquare(1)}
-				{this.renderSquare(2)}
-			</div>
-			<div className="board-row">
-				{this.renderSquare(3)}
-				{this.renderSquare(4)}
-				{this.renderSquare(5)}
-			</div>
-			<div className="board-row">
-				{this.renderSquare(6)}
-				{this.renderSquare(7)}
-				{this.renderSquare(8)}
-			</div>
+				<div className="board-row">
+					{this.renderSquare(0)}
+					{this.renderSquare(1)}
+					{this.renderSquare(2)}
+				</div>
+				<div className="board-row">
+					{this.renderSquare(3)}
+					{this.renderSquare(4)}
+					{this.renderSquare(5)}
+				</div>
+				<div className="board-row">
+					{this.renderSquare(6)}
+					{this.renderSquare(7)}
+					{this.renderSquare(8)}
+				</div>
 			</div>
 		);
 	}
@@ -51,12 +50,13 @@ class Game extends React.Component {
 			history: [{
 				squares: Array(9).fill(null)
 			}],
+			stepNumber: 0,
 			xIsNext: true,
 		};
 	}
 
 	handleClick(i) {
-		const history = this.state.history;
+		const history = this.state.history.slice(0, this.state.stepNumber + 1);
 		const current = history[history.length - 1];
 		const squares = current.squares.slice();
 		if (calculateWinner(squares) || squares[i]) {
@@ -67,13 +67,21 @@ class Game extends React.Component {
 			history: history.concat([{
 				squares: squares,
 			}]),
+			stepNumber: history.length,
 			xIsNext: !this.state.xIsNext,
 		});
 	}
 
+	jumpTo(step) {
+		this.setState({
+			stepNumber: step,
+			xIsNext: (step % 2) === 0,
+		})
+	}
+
 	render() {
 		const history = this.state.history;
-		const current = history[history.length - 1];
+		const current = history[this.state.stepNumber];
 		const winner = calculateWinner(current.squares);
 
 		const moves = history.map((step, move) => {
@@ -81,7 +89,7 @@ class Game extends React.Component {
 				'Go to move #' + move :
 				'Go to game start';
 			return (
-				<li>
+				<li key={move}>
 					<button onClick={() => this.jumpTo(move)}>{desc}</button>				
 				</li>
 			)
